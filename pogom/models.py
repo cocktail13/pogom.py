2bfef9c92cb89ed05ceb461b8fc479740e33fffa
cocktail13 <michaelerzengel13@gmail.com>
2016-07-29 19:04:58 -0500

    Update models.py

--- pogom/models.py
+++ pogom/models.py
@@ -13,7 +13,7 @@ from base64 import b64encode
 from . import config
 from .utils import get_pokemon_name, get_args, send_to_webhook
 from .transform import transform_from_wgs_to_gcj
-from .customLog import printPokemon
+from .customLog import printPokemon, logPokemonDb
 
 log = logging.getLogger(__name__)
 
@@ -226,21 +226,20 @@ def parse_map(map_dict, iteration_num, step, step_location):
 
     cells = map_dict['responses']['GET_MAP_OBJECTS']['map_cells']
     for cell in cells:
-        if config['parse_pokemon']:
-            for p in cell.get('wild_pokemons', []):
-                d_t = datetime.utcfromtimestamp(
-                    (p['last_modified_timestamp_ms'] +
-                     p['time_till_hidden_ms']) / 1000.0)
-                printPokemon(p['pokemon_data']['pokemon_id'], p['latitude'],
-                             p['longitude'], d_t)
-                pokemons[p['encounter_id']] = {
-                    'encounter_id': b64encode(str(p['encounter_id'])),
-                    'spawnpoint_id': p['spawnpoint_id'],
-                    'pokemon_id': p['pokemon_data']['pokemon_id'],
-                    'latitude': p['latitude'],
-                    'longitude': p['longitude'],
-                    'disappear_time': d_t
-                }
+        for p in cell.get('wild_pokemons', []):
+            d_t = datetime.utcfromtimestamp(
+                (p['last_modified_timestamp_ms'] +
+                 p['time_till_hidden_ms']) / 1000.0)
+            printPokemon(p['pokemon_data']['pokemon_id'],p['latitude'],p['longitude'],d_t)
+            logPokemonDb(p)
+            pokemons[p['encounter_id']] = {
+                'encounter_id': b64encode(str(p['encounter_id'])),
+                'spawnpoint_id': p['spawnpoint_id'],
+                'pokemon_id': p['pokemon_data']['pokemon_id'],
+                'latitude': p['latitude'],
+                'longitude': p['longitude'],
+                'disappear_time': d_t
+            }
 
                 webhook_data = {
                     'encounter_id': b64encode(str(p['encounter_id'])),
